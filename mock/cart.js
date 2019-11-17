2b59ef7b5ff6319e416cb92e88695a04d08112f7
zhangliqiang <wuyaping34@hotmail.com>
2019-11-17 21:34:49 +0800

    20191117a

--- mock/cart.js
+++ mock/cart.js
@@ -1,21 +1,29 @@
-const Mock = require('mockjs')
+const Mock = require("mockjs");
 
-let getProducts = (req) => {
+let getProducts = req => {
     // console.log(req.query);
     return Mock.mock({
         "data|3-10": [{
-            'id|+1': 1,
-            'title': "@ctitle()",
-            'price|100-9999.2': 1.00,
-            'sku|10-30': 1
+            "id|+1": 1,
+            title: "@ctitle()",
+            "price|100-9999.2": 1.0,
+            "sku|10-30": 1
         }]
-    })
+    });
+};
 
-}
+const pay = req => {
+    // console.log(req.query);
+    return Mock.mock({
+        "data|1": true
+    });
+};
 
 module.exports = {
-
     [`GET /api/getProducts`](req, res) {
-        res.status(200).json(getProducts(req))
+        res.status(200).json(getProducts(req));
+    },
+    [`POST /api/pay`](req, res) {
+        res.status(404).json(pay(req));
     }
-}
+};
